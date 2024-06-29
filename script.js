85cc838518ac901449c4ab2915385204d6e25c0e
Ananasaaa <kairyakn@gmail.com>
2024-06-29 18:28:40 +0300

    modified

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
-function deleteLetters(string, ...symbol) {
-    
-    let newStr = '';
 
-     for(let i = 0; i < string.length; i++) {
-         
-         for(let j = 0; j < symbol.length; j++) {
-             
-             if(string[i] !== symbol[j]) {
-                 
-                 if(newStr[i] === symbol[j + 1]){
-                     newStr += string.substring([i],[i + 1]);
-                     
-                     } else {
-                         continue;
-                     }
-                         
-                 } else {
-                     break;
-                 }
-        }   
-    }
-    return newStr;   
+function delLett(string, ...symbol){
+
+    let strArr = string.split('');
+
+     let newStr = strArr.filter((elem) => !symbol.includes(elem))
+
+    return newStr.join('');
 };
 
-let result = deleteLetters('zoomagazin', 'o', 'a', 'i');
+let result = delLett('javascript', 'a', 's');
 console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+// function deleteLetters(string, ...symbol) {
+    
+//     let newStr = '';
+
+//      for(let i = 0; i < string.length; i++) {
+         
+//          for(let j = 0; j < symbol.length; j++) {
+             
+//              if(string[i] !== symbol[j]) {
+                 
+//                  if(newStr[i] === symbol[j + 1]){
+//                      newStr += string.substring([i],[i + 1]);
+                     
+//                      } else {
+//                          continue;
+//                      }
+                         
+//                  } else {
+//                     break;
+//                  }
+//             } 
+//          }
+//     return newStr;   
+// };
+
+// let result = deleteLetters('zoomagazin', 'o', 'a', 'i');
+// console.log(result);
 
 
 
